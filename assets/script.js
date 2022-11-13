5126b4e1efa0cefc7d2d0a35e19e3b8c6e89b6cb
fabien moreno <fabienmoreno1331@yahoo.com>
2022-11-13 17:01:07 -0600

    var set up js

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var startContainer = document.getElementById('container-0');
+var questionOne = document.getElementById('container-1');
+var questionTwo = document.getElementById('container-2');
+var questionThree = document.getElementById('container-3');
+var questionFour = document.getElementById('container-4');
+var startBtn = document.getElementById('start-btn');
+var nextBtn = document.getElementById('next-btn');
